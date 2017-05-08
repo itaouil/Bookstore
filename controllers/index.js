1e488121c1e6691e3318345e84fd0d9fb7fcda92
Ilyass Taouil <itaouil95@gmail.com>
2017-05-08 19:46:11 +0200

    Added index dust view (krakenjs) and started working on routing.

--- controllers/index.js
+++ controllers/index.js
@@ -2,17 +2,16 @@
 
 var IndexModel = require('../models/index');
 
-
 module.exports = function (router) {
 
     var model = new IndexModel();
 
     router.get('/', function (req, res) {
-        
-        
+
+
         res.render('index', model);
-        
-        
+
+
     });
 
 };
