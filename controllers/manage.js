c298fe9d751cea8a4bd3ab9e922b6bff66e0f95b
Ilyass Taouil <itaouil95@gmail.com>
2017-05-10 08:42:36 +0200

    Controllers for edit/removal/add of books.

--- controllers/manage.js
+++ controllers/manage.js
@@ -23,7 +23,7 @@ module.exports = function (router) {
         });
     });
 
-    router.post('/books', function (req, res) {
+    router.post('/books/add', function (req, res) {
 
         // Get fields
         var title = req.body.title && req.body.title.trim();
@@ -35,13 +35,13 @@ module.exports = function (router) {
         var cover = req.body.cover && req.body.cover.trim();
 
         if (title === "" || price === "") {
-            res.redirect('/manage/books/add');
             res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
         }
 
         if (isNaN(price)) {
-            res.redirect('/manage/books/add');
             res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
         }
 
         var newBook = new Book({
@@ -58,14 +58,70 @@ module.exports = function (router) {
           if (err) {
             console.log(err);
           }
-          res.redirect('/manage/books/add');
           res.location('/manage/books/add');
+          res.redirect('/manage/books/add');
         });
 
     });
 
+    router.get('/books/edit/:id', function(req, res) {
+        Category.find({}, function(err, categories) {
+            if (err) throw err;
+            Book.findOne({_id: req.params.id}, function(err, book) {
+                if (err) throw err;
+                res.render('manage/books/edit', {book: book, categories: categories});
+            })
+        });
+    });
+
+    router.post('/books/edit/:id', function(req, res) {
+      // Get fields
+      var title = req.body.title && req.body.title.trim();
+      var category = req.body.category && req.body.category.trim();
+      var author = req.body.author && req.body.author.trim();
+      var publisher = req.body.publisher && req.body.publisher.trim();
+      var price = req.body.price && req.body.price.trim();
+      var description = req.body.description && req.body.description.trim();
+      var cover = req.body.cover && req.body.cover.trim();
+
+      if (title === "" || price === "") {
+          res.location('/manage/books/edit');
+          res.redirect('/manage/books/edit');
+      }
+
+      if (isNaN(price)) {
+          res.location('/manage/books/edit');
+          res.redirect('/manage/books/edit');
+      }
+
+      console.log("And the ID is:", req.params.id);
+
+      Book.update({_id: req.params.id}, {
+          title       : title,
+          category    : category,
+          description : description,
+          author      : author,
+          publisher   : publisher,
+          price       : price,
+          cover       : cover
+      }, function(err) {
+          if (err) throw err;
+          res.location('/manage/books');
+          res.redirect('/manage/books');
+      });
+
+    });
+
+    router.get('/books/delete/:id', function (req, res) {
+        Book.remove({_id: req.params.id}, function(err) {
+          if (err) throw err;
+          res.location('/manage/books');
+          res.redirect('/manage/books');
+        });
+    });
+
     router.get('/categories', function (req, res) {
-        res.render('/manage/categories/index');
+        res.render('manage/categories/index');
     });
 
 };
